6d57ee10af59f3f495c04302b9005df4f26c4841
Ali-Haider-NSU <mdalihaider800@gmail.com>
2023-12-13 23:45:25 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+        extend: {
+            colors: {
+                blue: '#1B7DFF'
+            },
+        },
+    },
+    plugins: [],
+}
